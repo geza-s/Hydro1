ffe2d93b211ea3864ab6d9673436fcfcd467af12
Geza Soldati <geza.soldati@gmail.com>
2021-10-11 16:38:39 +0200

    Finished computing part 2

--- part2.m
+++ part2.m
@@ -9,7 +9,7 @@ close all %close alla figures
 clc %clear the command window
 
 % -------------------------------------------------------------------------
-% # 1: Compute the Weibull plotting position
+%% # 1: Compute the Weibull plotting position
 % -------------------------------------------------------------------------
 
 % import the data from Part1 using the function load
@@ -17,6 +17,7 @@ load assignment1_output_part1.mat
 weibull_table = [];
 gumbel_table = []; %table to store gumpel method parameters
 
+%% All the computing:
 for rainfall_duration = D
     ranks = [1:size(annualMax, 1)]';
     h = sort(annualMax(:,rainfall_duration));
@@ -26,25 +27,25 @@ for rainfall_duration = D
         disp(rainfall_duration)
     end
 
-    %% Computing the empirical freqeuencies
+    %%% Computing the empirical freqeuencies
 
     empirical_freq = flip(ranks)/ranks(end);
 
     weibull_matrix = [weibull_matrix empirical_freq];
 
-    %% Computing weibul position
+    %%% Computing weibul position
 
     weibull_position = ranks/(length(ranks)+1);
 
     weibull_matrix = [weibull_matrix weibull_position];
 
-    %% Computing the reduced variable
+    %%% Computing the reduced variable
 
     yF = -log(-log(weibull_position));
 
     weibull_matrix = [weibull_matrix yF];
 
-    %% Gumpel parameters: moment methods
+    %%% Gumpel parameters: moment methods
 
     mh = mean(h);
     sh = std(h);
@@ -59,7 +60,7 @@ for rainfall_duration = D
     u_mm = mh-0.5772/alpha_mm;
     
 
-    %% Gumbel parameters : gumbel method
+    %%% Gumbel parameters : gumbel method
 
     %mF = mean(weibull_position); %mean of reduced variable ??????
     mYF = mean(yF);
@@ -72,7 +73,7 @@ for rainfall_duration = D
     gumbel_table = [gumbel_table vect_param];
     
     weibull_table = cat(3, weibull_table, weibull_matrix);
-    %% plottting comparision of gumbel functions
+    %%% plottting comparision of gumbel functions
     x_h = [1:120];
     %gumpel_mm = exp(-exp(-gumbel_table(1,3)*(x_h-gumbel_table(2,3))));
     %gumpel_gm = exp(-exp(-gumbel_table(3,3)*(x_h-gumbel_table(4,3))));
@@ -92,15 +93,66 @@ for rainfall_duration = D
     pause(1)
 end
 
+%%%weibull_table: [ranks h empirical_freq weibull_pos yF_reduced_variable] 
 %% Compute return period for each duration
-MTh = [];
-for i = [1:6]
-    N = length(h);
-    m = weibull_table(:,1,i);
-    disp(length(m))
-    Th = (N+1)./(N+1-m);
+
+clear MTh h Ph emp_Th;
+h = weibull_table(:,2,1);
+Ph = weibull_table(:,4,1);
+emp_Th = 1./(1-Ph); %Th associated to each empirical h vector ! 
+
+MTh = []; %matrice of Th from gumpel parameters 
+for i=[1:6]
+    h = weibull_table(:,2,i);
+    alpha = gumbel_table(3,i);
+    u = gumbel_table(4,i);
+    Ph = exp(-exp(-alpha*(h-u)));
+    Th = 1./(1-Ph);
     MTh = [MTh Th];
 end
+disp('OK')
 
+%% 
 
+MTT = [];
+T_th = [1:2:100];
+for i=[1:6]
+    alpha = gumbel_table(3,i);
+    u = gumbel_table(4,i);
+    d = 1-(1./T_th);
+    h = u - (1/alpha)*log(-log(d));
+    MTT = [MTT h'];
+end
+
+MTT = [MTT T_th'];
+disp('h related to return periods T based on gumbel : Ok')
+%% plot this shit
+
+close all;
+
+col = ['#0072BD', '#EDB120', '#EDB120', '#EDB120','#EDB120']; 
+for i=[1:6]
+    plot(Th, weibull_table(:,2,i), 'o');
+    hold on;
+    plot(MTT(:,7), MTT(:,i), '-')
+    pause(2)
+end
+xlabel('Return Period [Th]')
+ylabel('Rainfall depth [h]')
+title('Rainfall depth for each return period')
+
+%% Create Matric H_Gum
+H_Gum = [];
+Ti = [10,40,100]';
+for i=[1:6]
+    alpha = gumbel_table(3,i);
+    u = gumbel_table(4,i);
+    hi = u - (1/alpha)*log(-log(1-(1./Ti)));
+    H_Gum = [H_Gum hi];
+end
+disp('Matric with 10,40,100 return year: ok')
+%%
+T = Ti;
+save('assignment1_output_part2', 'H_Gum', 'T', 'D');
+disp('H_Gum, T and D saved')
 
